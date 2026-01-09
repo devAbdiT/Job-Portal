6160de0f1f76caac4f5eef7228fc784b298c5459
Abdi begna <abditadesse89@gmail.com>
2026-01-10 01:07:39 +0300

    Updating categories

--- admin-panel/categories-admins/update-category.php
+++ admin-panel/categories-admins/update-category.php
@@ -1,5 +1,34 @@
 <?php
-require "../../config/config.php"; ?>
+require "../../config/config.php";
+if (isset($_GET['id'])) {
+  $id = $_GET['id'];
+
+
+  $select = $conn->prepare("SELECT * FROM categories WHERE id=:id");
+  $select->execute([':id' => $id]);
+
+  $category = $select->fetch(PDO::FETCH_OBJ);
+
+  if (isset($_POST['submit'])) {
+    if (empty($_POST['name'])) {
+      echo "<script>alert('input is empty');</script>";
+    } else {
+      $name = $_POST['name'];
+      $update = $conn->prepare("UPDATE categories SET name =:name WHERE id=:id");
+      $update->execute([':name' => $name, ':id' => $id]);
+
+      header("location: " . ADMINURL . "/categories-admins/show-categories.php");
+    }
+  }
+} else {
+  header("location: http://localhost:3000/jobboard/404.php");
+}
+
+
+
+
+
+?>
 
 <?php require "../layouts/header.php";  ?>
 <div class="row">
@@ -7,10 +36,11 @@ require "../../config/config.php"; ?>
     <div class="card">
       <div class="card-body">
         <h5 class="card-title mb-5 d-inline">Update Categories</h5>
-        <form method="POST" action="" enctype="multipart/form-data">
+        <form method="POST" action="update-category.php?id=<?php echo $id; ?>" enctype="multipart/form-data">
           <!-- Email input -->
           <div class="form-outline mb-4 mt-4">
-            <input type="text" name="name" id="form2Example1" class="form-control" placeholder="name" />
+            <input type="text" name="name" id="form2Example1" class="form-control" placeholder="name"
+              value="<?php echo $category->name; ?>" />
 
           </div>
 
